3e881fa8e3c9fedf6fe46e4e0010dfbf4f50ce82
Ubuntu <ubuntu@ip-172-31-31-234.ap-southeast-2.compute.internal>
2024-12-12 13:39:03 +0000

    new

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js
 const express = require('express');
 const app = express();
 
@@ -5,12 +6,5 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Only start the server if the file is executed directly
-if (require.main === module) {
-  app.listen(3000, () => {
-    console.log('Server listening at http://localhost:3000');
-  });
-}
-
-module.exports = app; // Export the app for testing
+module.exports = app;  // Export the app for testing
 
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,12 @@
+// test/app.test.js
 const request = require('supertest');
-const app = require('../index');  // Import the app, not the running server
+const app = require('../index');  // Import the app
 
 describe('GET /', () => {
   it('should return Hello World', async () => {
     const response = await request(app).get('/');
-    expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World!');
+    expect(response.status).toBe(200);
   });
 });
 
